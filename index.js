2f125fd0d5231a9918e2efda5082ced500e24ce2
Kirthi Nandi <kirthi.nandi@gmail.com>
2022-07-20 08:28:28 -0700

    commit tree cbba45e9af35b6e80a68ccac4643e9fbed7b4e71

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(x){
   // Write your code here!
+  if (x <= 400) {
+    return "This one is on me!";
+  }
+  else if (x > 400 && x <= 2000) {
+    return "That will be twenty bucks.";
+  }
+  else if (x > 2000 && x <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  }
+  else {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city === "NYC") {
+    return "Ok, sounds good.";
+  }
+  else {
+    return "No go.";
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if (tip === "generous") {
+    return "Thank you so much.";
+  }
+  else if (tip === "not as generous") {
+    return "Thank you.";
+  }
+  else {
+    return "Bye.";
+  }
 }
